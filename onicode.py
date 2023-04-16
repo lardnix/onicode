3e04c49b93fc190a766ab843b922b23a2aac406b
Diego Fernando <df676423@gmail.com>
2023-04-16 14:22:46 -0400

    Add support for expressions in the language

--- onicode.py
+++ onicode.py
@@ -12,11 +12,17 @@ class OniTokenType(Enum):
     KEYWORD    = auto()
     SYMBOL     = auto()
     DATATYPE   = auto()
+    OPERATOR   = auto()
 
     EOF        = auto()
 
 class OniKeywordType(Enum):
-    FUNC = auto()
+    FUNC  = auto()
+    
+    TRUE  = auto()
+    FALSE = auto()
+    
+    NULL  = auto()
 
 class OniSymbolType(Enum):
     L_PAREN = auto()
@@ -32,9 +38,38 @@ class OniSymbolType(Enum):
 class OniDataType(Enum):
     NOTHING = auto()
 
+class OniOperator(Enum):
+    PLUS     = auto()
+    MINUS    = auto()
+    MULTIPLY = auto()
+    DIVIDE   = auto()
+    MODULO   = auto()
+    
+    BIT_AND  = auto()
+    BIT_OR   = auto()
+    BIT_XOR  = auto()
+    BIT_SHL  = auto()
+    BIT_SHR  = auto()
+    BIT_NOT  = auto()
+    
+    LOG_EQ   = auto()
+    LOG_NEQ  = auto()
+    LOG_LT   = auto()
+    LOG_GT   = auto()
+    LOG_LTE  = auto()
+    LOG_GTE  = auto()
+    LOG_AND  = auto()
+    LOG_OR   = auto()
+    LOG_NOT  = auto()
+
 
 ONI_KEYWORDS = {
-    "func": OniKeywordType.FUNC,
+    "func":  OniKeywordType.FUNC,
+    
+    "true":  OniKeywordType.TRUE,
+    "false": OniKeywordType.FALSE,
+    
+    "null":  OniKeywordType.NULL,
 }
 
 ONI_SYMBOLS = {
@@ -51,6 +86,31 @@ ONI_SYMBOLS = {
 
 ONI_DATATYPES = {
     "nothing": OniDataType.NOTHING,
+}
+
+ONI_OPERATORS = {
+    "+":  OniOperator.PLUS,
+    "-":  OniOperator.MINUS,
+    "*":  OniOperator.MULTIPLY,
+    "/":  OniOperator.DIVIDE,
+    "%":  OniOperator.MODULO,
+    
+    "&":  OniOperator.BIT_AND,
+    "|":  OniOperator.BIT_OR,
+    "^":  OniOperator.BIT_XOR,
+    "<<": OniOperator.BIT_SHL,
+    ">>": OniOperator.BIT_SHR,
+    "~":  OniOperator.BIT_NOT,
+    
+    "==": OniOperator.LOG_EQ,
+    "!=": OniOperator.LOG_NEQ,
+    "<":  OniOperator.LOG_LT,
+    ">":  OniOperator.LOG_GT,
+    "<=": OniOperator.LOG_LTE,
+    ">=": OniOperator.LOG_GTE,
+    "&&": OniOperator.LOG_AND,
+    "||": OniOperator.LOG_OR,
+    "!":  OniOperator.LOG_NOT,
 }
 
 @dataclass
@@ -83,6 +143,21 @@ class AST_OniExpression_Statement(AST):
     value: AST
 
 @dataclass
+class AST_OniEmpty_Statement(AST):
+    pass
+
+@dataclass
+class AST_OniBinary_Expression(AST):
+    lhs: AST
+    operator: OniOperator
+    rhs: AST
+
+@dataclass
+class AST_OniUnary_Expresstion(AST):
+    operator: OniOperator
+    value: AST
+
+@dataclass
 class AST_OniFunctionCall_Expression(AST):
     name: str
     arguments: List[AST]
@@ -90,6 +165,18 @@ class AST_OniFunctionCall_Expression(AST):
 @dataclass
 class AST_OniNumber_Expression(AST):
     value: int
+
+@dataclass
+class AST_OniTrue_Expression(AST):
+    pass
+
+@dataclass
+class AST_OniFalse_Expression(AST):
+    pass
+
+@dataclass
+class AST_OniNull_Expression(AST):
+    pass
 
 @dataclass
 class AST_OniArgument(AST):
@@ -171,15 +258,27 @@ class Lexer:
                 return OniToken(type=OniTokenType.IDENTIFIER, value=lexeme)
             
             lexeme = self.c
-            if lexeme in ONI_SYMBOLS:
+            if lexeme in ONI_SYMBOLS or lexeme + self.src[self.pos + 1] in ONI_SYMBOLS:
                 self.advance()
-                return OniToken(type=OniTokenType.SYMBOL, value=lexeme)
-            else:
-                self.advance()
-                lexeme += self.c
-                if lexeme in ONI_SYMBOLS:
+
+                double_symbol = lexeme + self.c
+
+                if double_symbol in ONI_SYMBOLS:
                     self.advance()
-                    return OniToken(type=OniTokenType.SYMBOL, value=lexeme)
+                    return OniToken(type=OniTokenType.SYMBOL, value=double_symbol)
+
+                return OniToken(type=OniTokenType.SYMBOL, value=lexeme)
+
+            elif lexeme in ONI_OPERATORS or lexeme + self.src[self.pos + 1] in ONI_OPERATORS:
+                self.advance()
+
+                double_operator = lexeme + self.c;
+
+                if double_operator in ONI_OPERATORS:
+                    self.advance()
+                    return OniToken(type=OniTokenType.OPERATOR, value=double_operator)
+
+                return OniToken(type=OniTokenType.OPERATOR, value=lexeme)
             
             throw_error(f"Invalid lexeme: {lexeme}")
 
@@ -215,13 +314,22 @@ class Parser:
 
                 if token_symbol_type != symbol_type:
                     throw_error(f"Invalid symbol type, expectd {symbol_type} but got {token_symbol_type}")
+            case OniTokenType.OPERATOR:
+                if token.type != token_type:
+                    throw_error(f"Invalid token type, expectd {token_type} but got {token.type}")
+
+                token_operator_type = ONI_OPERATORS[token.value]
+                operator_type = args[0]
+
+                if token_operator_type != operator_type:
+                    throw_error(f"Invalid operator type, expectd {operator_type} but got {token_operator_type}")
+
+
 
         self.token = self.lexer.next_token()
 
         return token
 
-    def number(self) -> AST_OniNumber_Expression:
-        return AST_OniNumber_Expression(value=int(self.eat(OniTokenType.NUMBER).value))
 
     def argument(self) -> AST_OniArgument:
         return AST_OniArgument(value=self.expression())
@@ -233,40 +341,171 @@ class Parser:
 
         self.eat(OniTokenType.SYMBOL, OniSymbolType.L_PAREN)
         
-        while not (self.token.type == OniTokenType.SYMBOL and ONI_SYMBOLS[self.token.value] == OniSymbolType.R_PAREN):
-            arguments.append(self.argument())
+        arguments.append(self.argument())
 
         self.eat(OniTokenType.SYMBOL, OniSymbolType.R_PAREN)
 
-        self.eat(OniTokenType.SYMBOL, OniSymbolType.SEMI)
-
         return AST_OniFunctionCall_Expression(name=name.value, arguments=arguments)
 
-    def expression(self) -> AST_OniExpression_Statement:
-        if self.token.type == OniTokenType.NUMBER:
-            return AST_OniExpression_Statement(value=self.number())
+    def number_expr(self) -> AST_OniNumber_Expression:
+        return AST_OniNumber_Expression(value=int(self.eat(OniTokenType.NUMBER).value))
 
-        return AST_OniExpression_Statement(value=self.function_call_expr())
+    def true_expr(self) -> AST_OniTrue_Expression:
+        self.eat(OniTokenType.KEYWORD, OniKeywordType.TRUE)
+        return AST_OniTrue_Expression()
+
+    def false_expr(self) -> AST_OniFalse_Expression:
+        self.eat(OniTokenType.KEYWORD, OniKeywordType.FALSE)
+        return AST_OniFalse_Expression()
+
+    def null_expr(self) -> AST_OniNull_Expression:
+        self.eat(OniTokenType.KEYWORD, OniKeywordType.NULL)
+        return AST_OniNull_Expression()
+
+    def primary_expr(self) -> AST:
+        if self.token.type == OniTokenType.KEYWORD:
+            keyword = ONI_KEYWORDS[self.token.value]
+
+            if keyword == OniKeywordType.TRUE: return self.true_expr()
+            if keyword == OniKeywordType.FALSE: return self.false_expr()
+            if keyword == OniKeywordType.NULL: return self.null_expr()
+        
+        if self.token.type == OniTokenType.NUMBER:
+            return self.number_expr()
+
+        if self.token.type == OniTokenType.SYMBOL and ONI_SYMBOLS[self.token.value] == OniSymbolType.L_PAREN:
+            self.eat(OniTokenType.SYMBOL, OniSymbolType.L_PAREN)
+            expr = self.expression()
+            self.eat(OniTokenType.SYMBOL, OniSymbolType.R_PAREN)
+
+            return expr
+        
+        return self.function_call_expr()
+
+    def unary_expr(self) -> AST:
+        if self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.PLUS | OniOperator.MINUS | OniOperator.BIT_NOT | OniOperator.LOG_NOT:
+                    self.eat(OniTokenType.OPERATOR, operator)
+
+                    return AST_OniUnary_Expresstion(operator=operator, value=self.unary_expr())
+
+        return self.primary_expr()
+
+    def bitwise_expr(self) -> AST:
+        ast = self.unary_expr()
+
+        while self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.BIT_AND | OniOperator.BIT_OR | OniOperator.BIT_XOR | OniOperator.BIT_SHL | OniOperator.BIT_SHR:
+                    self.eat(OniTokenType.OPERATOR, operator)
+                    ast = AST_OniBinary_Expression(lhs=ast, operator=operator, rhs=self.unary_expr())
+                case _:
+                    break;
+        return ast
+
+    def factor_expr(self) -> AST:
+        ast = self.bitwise_expr()
+
+        while self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.DIVIDE | OniOperator.MULTIPLY | OniOperator.MODULO:
+                    self.eat(OniTokenType.OPERATOR, operator)
+                    ast = AST_OniBinary_Expression(lhs=ast, operator=operator, rhs=self.bitwise_expr())
+                case _:
+                    break;
+        return ast
+
+    def term_expr(self) -> AST:
+        ast = self.factor_expr()
+
+        while self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.PLUS | OniOperator.MINUS:
+                    self.eat(OniTokenType.OPERATOR, operator)
+                    ast = AST_OniBinary_Expression(lhs=ast, operator=operator, rhs=self.factor_expr())
+                case _:
+                    break;
+        return ast
+
+    def relational_expr(self) -> AST:
+        ast = self.term_expr()
+
+        while self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.LOG_LT | OniOperator.LOG_GT | OniOperator.LOG_LTE | OniOperator.LOG_GTE:
+                    self.eat(OniTokenType.OPERATOR, operator)
+                    ast = AST_OniBinary_Expression(lhs=ast, operator=operator, rhs=self.term_expr())
+                case _:
+                    break;
+        return ast
+
+    def equality_expr(self) -> AST:
+        ast = self.relational_expr()
+
+        while self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.LOG_EQ | OniOperator.LOG_NEQ:
+                    self.eat(OniTokenType.OPERATOR, operator)
+                    ast = AST_OniBinary_Expression(lhs=ast, operator=operator, rhs=self.relational_expr())
+                case _:
+                    break;
+        return ast
+
+
+
+    def logical_expr(self) -> AST:
+        ast = self.equality_expr()
+
+        while self.token.type == OniTokenType.OPERATOR:
+            operator = ONI_OPERATORS[self.token.value]
+
+            match operator:
+                case OniOperator.LOG_AND | OniOperator.LOG_OR:
+                    self.eat(OniTokenType.OPERATOR, operator)
+                    ast = AST_OniBinary_Expression(lhs=ast, operator=operator, rhs=self.equality_expr())
+                case _:
+                    break;
+        return ast
+
+    def assignment_expr(self) -> AST:
+        #TODO: Update when include variables
+        return self.logical_expr()
+
+    def expression(self) -> AST_OniExpression_Statement:
+        return AST_OniExpression_Statement(value=self.assignment_expr())
 
 
     def statement(self) -> AST:
         if self.token.type == OniTokenType.IDENTIFIER:
             return self.expression()
 
-        throw_error(f"Expected token type identifier but got {self.token.value}")
-        return AST()
+        return AST_OniEmpty_Statement()
 
     def block(self) -> AST_OniBlock:
-        ast_block = AST_OniBlock(statements=[])
-
         self.eat(OniTokenType.SYMBOL, OniSymbolType.L_BRACE)
 
-        while not (self.token.type == OniTokenType.SYMBOL and ONI_SYMBOLS[self.token.value] == OniSymbolType.R_BRACE):
-            ast_block.statements.append(self.statement())
+        statements = [self.statement()]
+
+        while self.token.type == OniTokenType.SYMBOL and ONI_SYMBOLS[self.token.value] == OniSymbolType.SEMI:
+            self.eat(OniTokenType.SYMBOL, OniSymbolType.SEMI)
+            statements.append(self.statement())
 
         self.eat(OniTokenType.SYMBOL, OniSymbolType.R_BRACE)
 
-        return ast_block
+        return AST_OniBlock(statements=statements)
 
     def function_declaration(self) -> AST_OniFunction_Declaration:
         self.eat(OniTokenType.KEYWORD, OniKeywordType.FUNC)
@@ -317,8 +556,6 @@ class Generator:
         if self.argument_count < len(registers):
             return registers[self.argument_count]
 
-    def gen_AST_OniNumber_Expression(self, ast: AST_OniNumber_Expression):
-        self.src += f"  mov rax, {ast.value}\n"
 
     def gen_AST_OniArgument(self, ast: AST_OniArgument):
         self.generate(ast.value)
@@ -332,6 +569,121 @@ class Generator:
             self.generate(argument)
 
         self.src += f"  call {ast.name}\n"
+
+    def gen_AST_OniEmpty_Statement(self, ast: AST_OniEmpty_Statement):
+        pass
+
+    def gen_AST_OniNumber_Expression(self, ast: AST_OniNumber_Expression):
+        self.src += f"  mov rax, {ast.value}\n"
+
+    def gen_AST_OniTrue_Expression(self, ast: AST_OniTrue_Expression):
+        self.src += "  mov rax, 1\n"
+
+    def gen_AST_OniFalse_Expression(self, ast: AST_OniFalse_Expression):
+        self.src += "  mov rax, 0\n"
+
+    def gen_AST_OniNull_Expression(self, ast: AST_OniNull_Expression):
+        self.src += "  mov rax, 0\n"
+
+    def gen_AST_OniUnary_Expresstion(self, ast: AST_OniUnary_Expresstion):
+        self.generate(ast.value)
+
+        match ast.operator:
+            case OniOperator.PLUS:
+                self.src += "  xor rbx, rbx\n"
+                self.src += "  xchg rax, rbx\n"
+                self.src += "  add rax, rbx\n"
+            case OniOperator.MINUS:
+                self.src += "  xor rbx, rbx\n"
+                self.src += "  xchg rax, rbx\n"
+                self.src += "  sub rax, rbx\n"
+            case OniOperator.BIT_NOT:
+                self.src += "  not rax\n"
+            case OniOperator.LOG_NOT:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, 0\n"
+                self.src += "  cmove rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+
+    def gen_AST_OniBinary_Expression(self, ast: AST_OniBinary_Expression):
+        self.generate(ast.lhs)
+        self.src += "  push rax\n"
+        self.generate(ast.rhs)
+        self.src += "  pop rbx\n"
+        self.src += "  xchg rax, rbx\n"
+
+        match ast.operator:
+            case OniOperator.PLUS:
+                self.src += "  add rax, rbx\n"
+            case OniOperator.MINUS:
+                self.src += "  sub rax, rbx\n"
+            case OniOperator.MULTIPLY:
+                self.src += "  xor rdx, rdx\n"
+                self.src += "  mul rbx\n"
+            case OniOperator.DIVIDE:
+                self.src += "  xor rdx, rdx\n"
+                self.src += "  div rbx\n"
+            case OniOperator.MODULO:
+                self.src += "  xor rdx, rdx\n"
+                self.src += "  div rbx\n"
+                self.src += "  mov rax, rdx\n"
+            
+            case OniOperator.BIT_AND:
+                self.src += "  and rax, rbx\n"
+            case OniOperator.BIT_OR:
+                self.src += "  or rax, rbx\n"
+            case OniOperator.BIT_XOR:
+                self.src += "  xor rax, rbx\n"
+            case OniOperator.BIT_SHL:
+                self.src += "  mov rcx, rbx\n"
+                self.src += "  shl rax, cl\n"
+            case OniOperator.BIT_SHR:
+                self.src += "  mov rcx, rbx\n"
+                self.src += "  shr rax, cl\n"
+            
+            case OniOperator.LOG_EQ:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, rbx\n"
+                self.src += "  cmove rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+            case OniOperator.LOG_NEQ:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, rbx\n"
+                self.src += "  cmovne rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+            case OniOperator.LOG_LT:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, rbx\n"
+                self.src += "  cmovl rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+            case OniOperator.LOG_GT:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, rbx\n"
+                self.src += "  cmovg rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+            case OniOperator.LOG_LTE:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, rbx\n"
+                self.src += "  cmovle rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+            case OniOperator.LOG_GTE:
+                self.src += "  mov rcx, 0\n"
+                self.src += "  mov rdx, 1\n"
+                self.src += "  cmp rax, rbx\n"
+                self.src += "  cmovge rcx, rdx\n"
+                self.src += "  mov rax, rcx\n"
+            case OniOperator.LOG_AND:
+                self.src += "  and rax, rbx\n"
+            case OniOperator.LOG_OR:
+                self.src += "  or rax, rbx\n"
+
+
 
     def gen_AST_OniExpression_Statement(self, ast: AST_OniExpression_Statement):
         self.generate(ast.value)
